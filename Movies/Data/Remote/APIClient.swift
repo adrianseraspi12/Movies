317590ddc242ddbe2b799a23c0e9b2ca472dcfad
AJ Seraspi <adrianseraspi12@gmail.com>
2021-03-27 15:05:27 +0800

    Creates and implement query value types

--- Movies/Data/Remote/APIClient.swift
+++ Movies/Data/Remote/APIClient.swift
@@ -11,29 +11,28 @@ import Alamofire
 
 protocol Client {
     
+    //  A request to search for a movie in iTunes
     func requestSearchMovies(query: String,
-                             country: String,
-                             media: String) -> Future<[MovieList.Result], AFError>
+                             country: Country) -> Future<[MovieList.Result], AFError>
     
 }
 
 class APIClient: Client {
 
-    let baseUrlString = "https://itunes.apple.com/"
-    
-    var sessionManager: Session
+    private let baseUrlString = "https://itunes.apple.com/"
+    private var sessionManager: Session
     
     init(sessionManager: Session) {
         self.sessionManager = sessionManager
     }
     
-    func requestSearchMovies(query: String, country: String, media: String) -> Future<[MovieList.Result], AFError> {
+    func requestSearchMovies(query: String, country: Country) -> Future<[MovieList.Result], AFError> {
         
         //  Create a parameters for api client
         var params = Parameters()
         params["term"] = query
-        params["country"] = country
-        params["media"] = media
+        params["country"] = country.rawValue
+        params["media"] = Media.movie.rawValue
         
         //  Create request from Session
         let request = sessionManager.request(createUrl(withPath: "search"),
@@ -41,7 +40,7 @@ class APIClient: Client {
         
         ///  Using combine framework
         ///  Use future to produce a value for success or fail
-        ///  and execute the request in background thread (global())
+        ///  Execute the request in background thread (global())
         return Future { promise in
             request.responseDecodable(
                 of: MovieList.self,
@@ -64,7 +63,7 @@ class APIClient: Client {
 
 extension APIClient {
     
-    func createUrl(withPath: String = "") -> URL {
+    private func createUrl(withPath: String = "") -> URL {
         return URL(string: baseUrlString + withPath)!
     }
     
