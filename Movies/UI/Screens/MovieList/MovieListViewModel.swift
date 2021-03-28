990f8bdad3bdb1015ef4ecf3f5528da0f3af1dba
AJ Seraspi <adrianseraspi12@gmail.com>
2021-03-28 18:15:30 +0800

    Implement getting the persisted data if available

--- Movies/UI/Screens/MovieList/MovieListViewModel.swift
+++ Movies/UI/Screens/MovieList/MovieListViewModel.swift
@@ -14,27 +14,51 @@ class MovieListViewModel: ViewModel {
     private var api: Client
     private var localDb: Database
     private var cancellables = Set<AnyCancellable>()
-    
-    var searchQuery: String = ""
+    private var userDefaults: UserDefaults
     
     init(vc: View, userDefaults: UserDefaults, api: Client, localDb: Database) {
         self.vc = vc
         self.api = api
         self.localDb = localDb
+        self.userDefaults = userDefaults
     }
     
+    /// Get the persisted data from core data if available
     func setup() {
+        let searchQuery = self.userDefaults.string(forKey: "search_query") ?? ""
+        if (searchQuery.isEmpty) { return }
         
+        vc.setSearch(query: searchQuery)
+        
+        localDb.getAllMovies()
+            .sink { [weak self] (completion) in
+                switch completion {
+                case .finished:
+                    self?.vc.set(state: .loading(false))
+                    break
+                    
+                case .failure(let error):
+                    self?.vc.set(state: .loading(false))
+                    self?.vc.set(state: .error(error.localizedDescription))
+                    break
+                }
+            } receiveValue: { [weak self]  (movies) in
+                let listOfMainMovie = movies.map { $0.convertToMainMovies() }
+                self?.vc.set(state: .displayList(listOfMainMovie))
+            }.store(in: &cancellables)
+
     }
     
     func search(query: String) {
-        searchQuery = query
+        vc.set(state: .displayList([]))
         vc.set(state: .loading(true))
+        
+        //  Save the value of query
+        userDefaults.setValue(query, forKey: "search_query")
         
         //  Return Empty List
         if (query.isEmpty) {
             vc.set(state: .loading(false))
-            vc.set(state: .displayList([]))
             return
         }
         
@@ -52,7 +76,7 @@ class MovieListViewModel: ViewModel {
             } receiveValue: { [weak self] (result) in
                 //  Convert the MovieList.Result to MainMovie
                 let mainMovieList = result.map {
-                    self?.convertToMainMovie(model: $0) ?? MainMovies()
+                    $0.convertToMainMovies()
                 }
                 
                 //  Update the view and display the list
@@ -61,20 +85,12 @@ class MovieListViewModel: ViewModel {
                 //  Delete all movies in the database
                 self?.localDb.deleteAllMovies()
                 
-                //  Save a new data
+                //  Save app state
+                self?.userDefaults.setValue(AppState.list.rawValue, forKey: "app_state")
+                
+                //  Save a new list in database
                 self?.localDb.saveAllMovies(list: result)
             }.store(in: &cancellables)
 
-    }
-    
-    private func convertToMainMovie(model: MovieList.Result) -> MainMovies {
-        return MainMovies(id: model.id,
-                          imageUrl: model.imageUrl,
-                          previewUrl: model.previewUrl,
-                          trackName: model.trackName,
-                          trackPrice: model.trackPrice,
-                          currency: model.currency,
-                          genre: model.genre,
-                          longDescription: model.longDescription)
     }
 }
